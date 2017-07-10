e1f41c7a5b7f4c12281e7142648660b78a54bdb3
Noah Dowd <noah.c.dowd@gmail.com>
2017-07-10 16:36:17 -0400

    Initial Commit

---
+++ Nethack Client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nethack Client
+//
+//  Created by Noah Dowd on 7/10/17.
+//  Copyright © 2017 Eigengrau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
